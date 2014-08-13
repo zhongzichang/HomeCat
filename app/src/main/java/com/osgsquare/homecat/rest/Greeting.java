0475144ebe6ec49a9708b6adc69353a2b2a8035b
zhong zichang <zzc@zhongs-MacBook-Pro.local>
2014-08-13 08:35:37 +0800

    init

---
+++ app/src/main/java/com/osgsquare/homecat/rest/Greeting.java
@@ -0,0 +1,18 @@
+package com.osgsquare.homecat.rest;
+
+/**
+ * Created by zzc on 8/13/14.
+ */
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
